b53a2fc93170c488f324f76cabf6a93b18248d92
li3500764 <820901797@qq.com>
2025-11-06 17:49:11 +0800

    feat:排版有点问题，明天解决一下，2.44

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
+import LogoSection from "./components/LogoSection"
 import NavBar from "./components/NavBar"
+import ExperienceSection from "./sections/ExperienceSection"
+import FeatureCards from "./sections/FeatureCards"
 import Hero from "./sections/Hero"
 import ShowcaseSection from "./sections/ShowcaseSection"
+import TechStack from "./sections/TechStack"
 
 const App = () => {
   return (
@@ -8,6 +12,10 @@ const App = () => {
         <NavBar />
         <Hero />
         <ShowcaseSection />
+        <LogoSection />
+        <FeatureCards />
+        <ExperienceSection />
+        <TechStack />
     </>
   )
 }
